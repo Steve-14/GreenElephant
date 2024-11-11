988b2373a813cd4818272a519aff9069fd1e8c7c
Steve-14 <mistermole61@outlook.com>
2024-11-11 10:31:35 +0000

    Starting design for client

---
+++ greenelephantcraft/gec.js
@@ -0,0 +1,19 @@
+
+const pic = document.getElementById("ele");
+pic.addEventListener("mouseover", () => {
+    pic.style.borderStyle = "solid";
+    console.log("help me");
+})
+
+
+    const pix = document.getElementById("ele");
+    pic.addEventListener("mouseout", () => {
+        pic.style.borderStyle = "dashed";
+    console.log("help them all");
+})
+
+
+//const art = document.getElementsByClassName("");
+    //art.addEventListener("mouseover", () => {
+        //transform:(2,2) = "dashed";
+    //console.log("help them all");
